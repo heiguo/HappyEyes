31e9379b7995d4ddab79e181fab072d3bfc61538
Roger <roger@ru1104.(none)>
2012-06-13 11:28:15 +0800

    fix increasing width bug

--- 1.js
+++ 1.js
@@ -1,10 +1,23 @@
 var MAGIC_TEXT_LENGTH = 30;
 
+// util function: get font size of an element
+function getElemWidth(elem) {
+  if (document.defaultView && document.defaultView.getComputedStyle) {
+    return document.defaultView.getComputedStyle(elem, null).getPropertyValue("width");
+  } else {
+    return elem.style["width"];
+  }
+};
+
+
 function reStyle(elem) {
   //elem.style.fontFamily = "Corbel, Georgia, 'Microsoft Yahei'";
   elem.style.fontSize = '18px';
   elem.style.lineHeight = '175%';
-  elem.style.width = '700px';
+  var width = parseFloat(getElemWidth(elem));
+  if (width > 600) {
+    elem.style.width = '600px';
+  }
 //  elem.style.textAlign = 'justify';
 }
 
